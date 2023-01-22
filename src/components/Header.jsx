ad4acf6843f5b7356d5f2a6b12122bb8a4cf53e8
AlexaNem0 <vikalyeksyeyev@gmail.com>
2023-01-22 15:38:36 -0600

    header fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="  flex flex-col gap-2 mt-6">
       <h1 className=" font-bold text-5xl  ">
-        PING <span className="text-[#4278FF]">.</span>
+        PING<span className="text-[#4278FF]">.</span>
       </h1>
       <h2 className="text-[#969696] text-3xl md:text-5xl">
         We are launching <span className="font-bold text-black ">soon!</span>
